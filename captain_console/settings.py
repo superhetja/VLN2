3763ee5110cc4bc970c8d97009289b3f58e0fb66
holmfridur <holmfridurh17@ru.is>
2020-05-02 23:48:12 +0000

    fullt i game

--- captain_console/settings.py
+++ captain_console/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'captain_console.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wxjejgme',
+        'USER': 'wxjejgme',
+        'PASSWORD': 'eed2uMqw_weoePSYYE_gZkJ_xtuCAJVj',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
