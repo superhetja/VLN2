7778f66235e187997270daa2dd261262ad88b8bc
holmfridur <holmfridurh17@ru.is>
2020-05-02 15:20:14 +0000

    game app

--- captain_console/settings.py
+++ captain_console/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'captainConsole.apps.CaptainconsoleConfig',
 ]
 
 MIDDLEWARE = [
